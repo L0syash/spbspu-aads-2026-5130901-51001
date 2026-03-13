553d221c4ad1562b23cd7c5d423db5d3b2c56b12
L0syash <losev_timofey07@mail.ru>
2026-03-13 23:23:43 +0300

    just PLEASE

--- losev.timofey/S1/main.cpp
+++ losev.timofey/S1/main.cpp
@@ -39,14 +39,17 @@ int main() {
   std::string name;
   bool overflow_detected = false;
 
-  while (std::cin >> name && !overflow_detected) {
+  while (std::cin >> name) {
     NamedList seq;
     seq.name = name;
 
     int num;
-    while (std::cin.peek() != '\n' && std::cin.peek() != EOF && !overflow_detected) {
+    while (std::cin.peek() != '\n' && std::cin.peek() != EOF) {
       if (!(std::cin >> num)) {
         overflow_detected = true;
+        std::cin.clear();
+        std::string token;
+        std::cin >> token;
         break;
       }
       seq.numbers.push_front(num);
@@ -56,20 +59,17 @@ int main() {
       std::cin.ignore();
     }
 
-    if (!overflow_detected) {
-      seq.numbers = reverse(seq.numbers);
-      sequences.push_front(seq);
-    }
+    seq.numbers = reverse(seq.numbers);
+    sequences.push_front(seq);
   }
-  if (overflow_detected) {
-    std::cerr << "overflow" << "\n";
-    return 1;
-  }
+
   sequences = reverse(sequences);
+
   if (sequences.empty()) {
     std::cout << "0" << "\n";
     return 0;
   }
+
   bool first = true;
   for (List< NamedList >::iterator it = sequences.begin(); it != sequences.end(); ++it) {
     if (!first) std::cout << " ";
@@ -77,24 +77,15 @@ int main() {
     first = false;
   }
   std::cout << "\n";
+
   size_t max_len = 0;
   for (List< NamedList >::iterator seq_it = sequences.begin(); seq_it != sequences.end(); ++seq_it) {
     size_t len = length(seq_it->numbers);
     if (len > max_len) max_len = len;
   }
-  if (max_len == 0) {
-    std::cout << "0" << "\n";
-    return 0;
-  }
-  List< int > row_sums;
-  for (size_t i = 0; i < max_len; ++i) {
-    row_sums.push_front(0);
-  }
-  row_sums = reverse(row_sums);
+
   for (size_t i = 0; i < max_len; ++i) {
     first = true;
-    List< int >::iterator sum_it = row_sums.begin();
-    for (size_t pos = 0; pos < i; ++pos) ++sum_it;
     for (List< NamedList >::iterator seq_it = sequences.begin(); seq_it != sequences.end(); ++seq_it) {
       size_t len = length(seq_it->numbers);
       if (i < len) {
@@ -102,18 +93,42 @@ int main() {
         if (!first) std::cout << " ";
         std::cout << value;
         first = false;
-        *sum_it += value;
       }
     }
     std::cout << "\n";
   }
-  first = true;
-  for (List< int >::iterator sum_it = row_sums.begin(); sum_it != row_sums.end(); ++sum_it) {
-    if (!first) std::cout << " ";
-    std::cout << *sum_it;
-    first = false;
-  }
-  std::cout << "\n";
 
-  return 0;
+  if (!overflow_detected) {
+    List< int > row_sums;
+    for (size_t i = 0; i < max_len; ++i) {
+      row_sums.push_front(0);
+    }
+    row_sums = reverse(row_sums);
+
+    for (size_t i = 0; i < max_len; ++i) {
+      List< int >::iterator sum_it = row_sums.begin();
+      for (size_t pos = 0; pos < i; ++pos) ++sum_it;
+
+      for (List< NamedList >::iterator seq_it = sequences.begin(); seq_it != sequences.end(); ++seq_it) {
+        size_t len = length(seq_it->numbers);
+        if (i < len) {
+          int value = get_element_at(seq_it->numbers, i);
+          *sum_it += value;
+        }
+      }
+    }
+
+    first = true;
+    for (List< int >::iterator sum_it = row_sums.begin(); sum_it != row_sums.end(); ++sum_it) {
+      if (!first) std::cout << " ";
+      std::cout << *sum_it;
+      first = false;
+    }
+    std::cout << "\n";
+    
+    return 0;
+  } else {
+    std::cerr << "overflow" << "\n";
+    return 1;
+  }
 }
